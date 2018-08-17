09825cfee7d0cb49c42d6d8405383f4e514ab774
Student <example@example.com>
2018-08-17 11:09:04 -0700

    Enable reverse order and add function to display list to the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
 $(function(){
   $("#submit-button").click(function(){
     var userNumber = $('#number-input').val()
-    console.log(boopify(userNumber))
+    displayBoopedList(userNumber)
   })
 })
 
 function boopify(userInput) {
   var output = []
   var toList = parseInt(userInput)
-
   for (var i=0; i<=toList; i++) {
-    var num = i.toString()
-    var toPass = num
+    var numString = i.toString()
+    var toPass = numString
     if (i > 0 && i % 3 === 0) {
       toPass = "I'm sorry, Dave. I'm afraid I can't do that."
-    } else if (num.includes("1")) {
+    } else if (numString.includes("1")) {
       toPass = "Boop!"
-    } else if (num.includes("0")) {
+    } else if (numString.includes("0")) {
       toPass = "Beep!"
     }
     output.push(toPass)
@@ -25,4 +24,11 @@ function boopify(userInput) {
     output = output.reverse()
   }
   return output
+}
+function displayBoopedList(userNumber) {
+  $('#display').html("")
+  var list = boopify(userNumber)
+  list.forEach(function(item,i){
+    $('#display').append(item+'<br />')
+  })
 }
