56a7b976b1335ec4e07b96eb4210444d26ad6cd3
Student <example@example.com>
2018-08-17 11:57:50 -0700

    Add option for fancy reveal

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 $(function(){
   $("#submit-button").click(function(){
-    var userNumber = $('#number-input').val()
-    displayBoopedList(userNumber)
+    displayBoopedList($('#number-input').val())
   })
 })
 
@@ -28,7 +27,26 @@ function boopify(userInput) {
 function displayBoopedList(userNumber) {
   $('#display').html("")
   var list = boopify(userNumber)
+  var delay = 0;
   list.forEach(function(item,i){
-    $('#display').append(item+'<br />')
+    var extraClass = ""
+    if (i % 2 === 0) {
+      extraClass = " gray"
+    }
+    $('#display').append('<div id="num-'+i+'" class="display-number'+extraClass+'"><span>'+item+'</span></div>')
+
   })
+  if (document.getElementById('fancyCheck').checked) {
+    $(".display-number").css({
+      'opacity' : 0
+    })
+    console.log("fancy")
+    var delay = 0
+    list.forEach(function(item,i){
+      $('#num-'+i).animate({
+        'opacity' : 1,
+      },delay)
+      delay += 80
+    })
+  }
 }
